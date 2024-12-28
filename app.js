c6bcae4d342947d47945629423602247c045dab6
shery <sherypagal123@gmail.com>
2024-12-28 17:46:21 +0500

    project added

---
+++ app.js
@@ -0,0 +1,139 @@
+
+
+// CHAPTER NO: 1
+
+// Question 1
+
+// let student =[]
+
+// alert(student)
+
+// Question 2
+
+// let a =[]
+
+// alert(a)
+
+// Question 3
+
+// let initialize =["Hello","World","Javaschript","Programming"]
+
+// alert(initialize)
+
+// Question 4
+
+// let num =[1,2,3,4,5]
+
+// alert(num)
+
+// Question 5
+
+// let a =[true,false,true,false,true,false]
+
+// alert(a)
+
+// Question 6
+
+// let a = [12,"abcd",true,false,true,134,"abc"]
+
+// alert(a)
+
+// Question 7
+
+//  let b= ["SSC","HSC","BCS","BS","BCOM","MS","M","PHIL","PHD"]
+
+//  alert(b)
+
+// Question 8
+
+// let naMe =["Michael", "John", "Tony"]
+//  let src =[320,230,480]
+// let num1 =[320]
+// let num2 =[230]
+// let num3 =[480]
+// let num=[500]
+// // for(let i=0; i<naMe.length; i++){
+// //     let perc = (num / num1 ) *100
+// // }
+// let  percentage1=(num1/num*100)
+// let  percentage2=(num2/num*100)
+// let  percentage3=(num3/num*100)
+// document.write(`<h1> Score: ${naMe[0]}  ${src[0]} Percentage: ${percentage1}%`) 
+// document.write(`<h1> Score: ${naMe[1]}  ${src[1]} Percentage: ${percentage2}% `) 
+// document.write(`<h1> Score: ${naMe[2]}  ${src[2]} Percentage: ${percentage3}% `) 
+
+// Question 9
+
+// let colors=["red","green","blue","yellow"]
+
+// document.write(`<h1>Colors: <br> ${colors}</h1>`)
+
+// let colors2=prompt("Enter a 1st color to add to the start  of the array:")
+// let colors3=prompt("Enter a 2st color to add to the start of the array:")
+
+// colors.unshift(colors2)
+// colors.unshift(colors3)
+// document.write(`<h1>Colors: <br> ${colors}</h1>`)
+
+// let colors4=prompt("Enter a color to add to the end of the array:")
+
+// colors.push(colors4)
+// document.write(`<h1>Colors: <br> ${colors}</h1>`)
+
+// colors.shift()
+// document.write(`<h1>Colors: <br> ${colors}</h1>`)
+
+// colors.pop()
+// document.write(`<h1>Colors: <br> ${colors}</h1>`)
+
+
+
+// Question 10
+
+// let student =[320,230,480,120]
+
+// document.write(`<h1>Scores of Student: ${student}</h1> `)
+// document.write(`<h1>Ordered Scores of Student: ${student[3]},${student[1]},${student[0]},${student[2]}</h1>`)
+
+// Question 11
+
+// let a=["Karachi","Lahore","islamabad","Quetta","Peshawar"]
+
+// document.write(`<h1>Citise list: </h1> `)
+// document.write(`<h1> ${a} </h1> `)
+// document.write(`<h1>Selected cities list: </h1> `)
+// document.write(`<h1> ${a[2]},${a[3]} </h1`)
+
+// Question 12
+
+// let cat =["This","is","my","cat"]
+
+// let World=cat.join(' ');
+
+// document.write(`<h1>Array: <br> ${cat}</h1>`)
+
+// document.write(`<h1>string: <br> ${World}</h1>`)
+
+// Question 13
+
+// let a =["Keyboard","mous","printer","monitor"]
+
+// document.write(`<h1>Devices: <br> ${a}</h1>`)
+// document.write(`<h1>Out: <br> ${a[0]}</h1>`)
+// document.write(`<h1>Out: <br> ${a[1]}</h1>`)
+// document.write(`<h1>Out: <br> ${a[2]}</h1>`)
+// document.write(`<h1>Out: <br> ${a[3]}</h1>`)
+
+// Question 14
+
+//  let a =["Keyboard","mous","printer","monitor"]
+
+// document.write(`<h1>Devices: <br> ${a}</h1>`)
+// document.write(`<h1>Out: <br> ${a[3]}</h1>`)
+// document.write(`<h1>Out: <br> ${a[2]}</h1>`)
+// document.write(`<h1>Out: <br> ${a[1]}</h1>`)
+// document.write(`<h1>Out: <br> ${a[0]}</h1>`)
+
+
+// Question 15
+
